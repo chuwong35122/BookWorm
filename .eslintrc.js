ed55ee79b59601a4a11bafa48f967988603e6750
chuwong35122 <chuwong35122@gmail.com>
2022-03-01 23:48:19 +0700

    deps: migrate to NativeBase

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
       files: ['*.ts', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
+        'no-inline-styles': 'off',
         'no-shadow': 'off',
         'no-undef': 'off',
       },
--- App.tsx
+++ App.tsx
@@ -6,40 +6,46 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Home from './src/screens/Home';
 import NewBook from './src/screens/NewBook';
 import {RootStackParamList} from './src/navigation/types';
+import {extendTheme, NativeBaseProvider} from 'native-base';
 
 const App = () => {
   // const isDarkMode = useColorScheme() === "dark";
 
   LogBox.ignoreLogs([
     "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
+    'NativeBase: The contrast ratio of 1:1 for darkText on transparent',
   ]);
+
+  const nativeBaseTheme = extendTheme({});
 
   const Stack = createNativeStackNavigator<RootStackParamList>();
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Home}
-            options={{headerShown: false}}
-          />
-          <Stack.Screen
-            name="NewBook"
-            component={NewBook}
-            options={{
-              title: 'Add a New Book',
-              headerStyle: {
-                backgroundColor: 'rgb(180,221,227)',
-              },
-              headerTintColor: '#0d47a1',
-              headerTitleStyle: {
-                fontWeight: '500',
-              },
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <NativeBaseProvider theme={nativeBaseTheme}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="NewBook"
+              component={NewBook}
+              options={{
+                title: 'Add a New Book',
+                headerStyle: {
+                  backgroundColor: 'rgb(180,221,227)',
+                },
+                headerTintColor: '#0d47a1',
+                headerTitleStyle: {
+                  fontWeight: '500',
+                },
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
     </SafeAreaProvider>
   );
 };
