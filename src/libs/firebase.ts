66f3b15ffe8497b7ab25fa08f99e5d75d60d8b31
chuwong35122 <chuwong35122@gmail.com>
2022-03-02 08:55:33 +0700

    deps: add Firebase

---
+++ src/libs/firebase.ts
@@ -0,0 +1,15 @@
+import {getApps, initializeApp} from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAng0Hm2cOGax1nEMw8OfGtiVHOK6Q1BnA',
+  authDomain: 'bookself-f8efe.firebaseapp.com',
+  projectId: 'bookself-f8efe',
+  storageBucket: 'bookself-f8efe.appspot.com',
+  messagingSenderId: '388067965137',
+  appId: '1:388067965137:web:8722494454becb73f2adf0',
+  measurementId: 'G-1HGC4CSGGK',
+};
+
+if (!getApps().length) {
+  initializeApp(firebaseConfig);
+}
