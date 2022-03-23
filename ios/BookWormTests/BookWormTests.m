1306ebb4042cbeef521c0fab350e07feb2948789
chuwong35122 <chuwong35122@gmail.com>
2022-03-23 21:37:40 +0700

    chore: rebuilt android project

--- ios/BookWormTests/BookWormTests.m
+++ ios/BookWormTests/BookWormTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BookSelfTests : XCTestCase
+@interface BookWormTests : XCTestCase
 
 @end
 
-@implementation BookSelfTests
+@implementation BookWormTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
